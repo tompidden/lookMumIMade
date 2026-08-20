0f0985bda8cbba5f307e52071e8be05f99a72039
tompidden <150662485+tompidden@users.noreply.github.com>
2026-08-20 15:53:22 +0100

    Update config.ts
    Instagram "" added

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   social: {
     email: "tpidden@yahoo.com",
     linkedin: "https://www.linkedin.com/in/thomas-pidden/",
-    instagram: "https://instagram.com/tompidde
+    instagram: "https://instagram.com/tompidde/"
   },
   aboutMe:
     "I'm a Product Designer passionate about creating intuitive, user-centered digital experiences. I combine research, strategy, and craft to design products that solve real problems and delight the people who use them. From wireframes to high-fidelity prototypes, I enjoy working closely with cross-functional teams to bring ideas to life, iterating based on user feedback to build products that are both beautiful and functional.",
